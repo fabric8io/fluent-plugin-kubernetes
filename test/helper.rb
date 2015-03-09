15b762148c14eee2e0f3d44088ded22c2528bc86
Jimmi Dyson <jimmidyson@gmail.com>
2015-03-09 13:16:44 +0000

    Update to keep test artifacts for CircleCI

--- test/helper.rb
+++ test/helper.rb
@@ -16,8 +16,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+require "simplecov"
 require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join("..", "..", "..", ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+end
+
+SimpleCov.start do
+  formatter SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    CodeClimate::TestReporter::Formatter
+  ]
+end
 
 begin
   require "bundler"
